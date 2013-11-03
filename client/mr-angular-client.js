1dfe76623d90ec704eff39ac92fc393979816e4e
Jiri Spac <jis@censhare.de>
2013-11-03 16:59:41 +0100

    client part starts shaping up

--- client/mr-angular-client.js
+++ client/mr-angular-client.js
@@ -1,24 +1,33 @@
 angular.module('Moonridge', []).factory('$MR', function $MR($rpc, storage, $q) {
     function Moonridge(backendUrl) {
         var self = this;
-        $rpc.connect('http:'+ backendUrl);
+        var models = {};
+		$rpc.connect('http:'+ backendUrl);
         self.ready = $q.defer();
         $rpc.loadChannel('Moonridge').then(function (mrChnl) {
             mrChnl.getModels().then(self.ready.resolve);
         });
-		this.getChannel = function (name) {
-			return self.ready.then(function (obj) {
+		this.getModel = function (name) {
+			var model = {deferred: $q.defer()};
+			models[name] = model
+			self.ready.then(function (serverModels) {
 
-				$rpc.expose('MR-' + name, {
-					pub: function (param) {
-						return 'whatever you need from client returned ' + param;
-					}
-				}).then(
-					function (channel) {
-						console.log(" client channel ready");
-					}
-				);
-			})
+				var promises = {
+					client: $rpc.expose('MR-' + name, {
+						pub: function (doc, eventName) {
+							//TODO fire model event in the client
+						}
+					}),
+					server: $rpc.loadChannel('MR-' + name)
+				};
+
+
+				$q.all(promises).then(function (chnlPair) {
+					model.deferred.resolve(chnlPair.server);
+				})
+			});
+			return model.deferred.promise;
+
 		};
         return this;
     };

2b1cc73d4f5b523702a27d3b99c8c5a2f69baa4f
Jiří Špác <capajj@gmail.com>
2013-11-17 11:11:28 +0100

    little auth related refactoring

--- main.js
+++ main.js
@@ -2,47 +2,20 @@ var rpc = require('socket.io-rpc');
 var _ = require('lodash');
 var MRModel = require('./mr-model');
 
-var defIOSetter = function (io) {
+var defIOSetter = function (io, authFn) {
 	io.set('log level', 1);
 	io.set('transports', [ 'websocket']);
 	//    io.set('heartbeats', false);  // we would like this, but it does not work like this
+
+    if (authFn) {
+        io.set('authorization', authFn);
+    }
 };
 
-/**
- * @param server
- * @param app
- * @param {Object} opts
- * @param {Object} opts.auth
- * 		example: {global: function(handshake, CB){CB()}, methods: {method: }}
- * @param {Function} opts.ioSetter function for setting up socket io
- * @returns {Function}
- */
-module.exports = function (mongoose, server, app, opts) {
-	var io = require('socket.io').listen(server);
+var authFn;
 
-	app.get('/moonridge-angular-client.js', function (req, res) { //exposed client file
-		res.sendfile('/node_modules/moonridge/client/moonridge-angular-client.js');
-	});
+var init = function (mongoose) {
 
-    app.get('/moonridge-angular-client-rpcbundle.js', function (req, res) { //exposed client file
-		res.sendfile('/node_modules/moonridge/client/moonridge-angular-client-rpcbundle.js');
-	});
-
-    if (_.isFunction(opts && opts.ioSetter)) {
-        opts.ioSetter(io)
-	} else {
-		defIOSetter(io);
-	}
-
-    rpc.createServer(io, app);
-
-//    rpc.expose('Moonridge', {
-//        getModels: function () {
-//			return mongoose.models;
-//        }
-//    });
-
-	var authFn;
 	/**
 	 *
 	 * @returns {MRModel}
@@ -53,7 +26,8 @@ module.exports = function (mongoose, server, app, opts) {
 				opts = {};
 			}
 			if (opts.authFn) {
-				throw new Error('When global auth method is defined, individual model auth methods must not be defined.');
+				//TODO think about this
+                throw new Error('When global auth method is defined, individual model auth methods must not be defined.');
 			}
 			opts.authFn = authFn;
 		}
@@ -81,3 +55,44 @@ module.exports = function (mongoose, server, app, opts) {
 
 	return {model: regNewModel, userModel: registerUserModel};
 };
+
+/**
+ * @param server
+ * @param app
+ * @param {Object} opts
+ * @param {Object} opts.auth
+ * 		example: {global: function(handshake, CB){CB()}, methods: {method: }}
+ * @param {Function} opts.ioSetter function for setting up socket io
+ * @returns {SocketNamespace} master socket namespace
+ */
+var createServer = function (server, app, opts) {
+    var io = require('socket.io').listen(server);
+
+    app.get('/moonridge-angular-client.js', function (req, res) { //exposed client file
+        res.sendfile('/node_modules/moonridge/client/moonridge-angular-client.js');
+    });
+
+    app.get('/moonridge-angular-client-rpcbundle.js', function (req, res) { //exposed client file
+        res.sendfile('/node_modules/moonridge/client/moonridge-angular-client-rpcbundle.js');
+    });
+
+    io.configure(function (){
+        if (_.isFunction(opts && opts.ioSetter)) {
+            opts.ioSetter(io)
+        } else {
+            defIOSetter(io, opts.authFn);
+        }
+    });
+
+
+    return rpc.createServer(io, app);
+
+//    rpc.expose('Moonridge', {
+//        getModels: function () {
+//			return mongoose.models;
+//        }
+//    });
+
+};
+
+module.exports = {init: init, createServer: createServer};
--- test/server.js
+++ test/server.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 var _ = require('lodash');
 var locals = require('./localVariables.json');
-var Schema = mongoose.Schema;
 var ObjectId = mongoose.Types.ObjectId;
-var MRinit = require('../main');
+var Moonridge = require('../main');
 var express = require('express');
 var app = module.exports = express();
 
@@ -25,12 +24,9 @@ app.get('/moonridge-angular-client-rpcbundle.js', function (req, res) { //expose
     res.sendfile('./client/moonridge-angular-client-rpcbundle.js');
 });
 
-var MR = MRinit(mongoose, server, app);
-
 app.get('*', function (req, res) {
     res.sendfile('./test/index.html');
 });
-
 
 mongoose.connect(locals.connString, function (err) {
     // if we failed to connect, abort
@@ -41,25 +37,16 @@ mongoose.connect(locals.connString, function (err) {
     }
 });
 
-var user = MR.userModel({name: String, age: Number},{
-	authFn: function (handshake, CB) {
-		var socket = this;
-		user.model.find({name:handshake}).then(function (user) {
-			socket.user = user;
-			CB(true);
-		}, function (err) {
-			console.log("auth error " + err);
-			CB(false);
-		})
-	}
-});
+var MR = Moonridge.init(mongoose);
+
+var user = MR.userModel({name: String, age: Number});
 
 //just run once to have a user
-//user.model.create({
-//	name: 'capaj'
-//}).then(function (obj) {
-//		console.log("capaj created");
-//	});
+user.model.create({
+	name: 'capaj'
+}).then(function (obj) {
+		console.log("capaj created");
+	});
 
 
 var Fighter = MR.model('fighter', {
@@ -112,4 +99,28 @@ Fighter.model.create({
 //                console.log(doc.name + " saved");
 //            });
 //        c++;
+//    }, 3000);    var c = 0;
+//
+//    setInterval(function () {
+//        Fighter.model.create({
+//            name: 'gold cloak ' + c
+//            , health: 30
+//            , born: new Date()
+//        }).then(function (doc) {
+//                console.log(doc.name + " saved");
+//            });
+//        c++;
 //    }, 3000);
+
+Moonridge.createServer(server, app, {
+    authFn: function (handshake, CB) {
+    var socket = this;
+    user.model.find({name:handshake}).then(function (user) {
+        socket.user = user;
+        CB(true);
+    }, function (err) {
+        console.log("auth error " + err);
+        CB(false);
+    })
+}
+});

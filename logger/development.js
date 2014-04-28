5c90cb72aac87149463a15f960bd8257473c1b50
Jiri Spac <jiri.spac.contractor@avg.com>
2014-04-28 14:27:11 +0200

    gh pages first commit

--- logger/development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    logOpts: {
-        level: "info",
-        colorize: true,
-        timestamp: true
-    }
-};
--- utils/user-model-base.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	privilige_level: { type: Number, default: 1, min:0, max: 50, permissions: {w: 49, r: 0}}
-};

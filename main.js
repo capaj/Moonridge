c049c46028901ea849aa8cb99699bb11192af0c1
Jiri Spac <capajj@gmail.com>
2014-05-18 23:30:58 +0200

    updated dependencies

--- main.js
+++ main.js
@@ -5,116 +5,114 @@ var userModel;
 var toCallOnCreate = [];
 var logger = require('./logger/logger');
 
-
-var init = function (mongoose) {
-
-	/**
-	 * @param {String} name
-	 * @param {Object} schema
-	 * @param {Object} opts
-	 * @param {Function} opts.checkPermission function which should return true/false depending if the connected socket has/hasn't priviliges
-	 * @param {Object} opts.permissions with 4 properties: "C", "R","U", "D" which each represents one type of operation,
-     *                                  values should be numbers indicating which level of privilige is needed
-
-	 * @returns {MRModel}
-	 */
-    function regNewModel(name, schema, opts) {
-
-        var model = MRModel.apply(mongoose, arguments);
-        toCallOnCreate.push(model._exposeCallback);
-
-        return model;
-    }
-
-	/**
-	 *
-	 * @param schemaExtend
-	 * @param {Object} opts
-	 * @param {Function} opts.authFn will be set as default authFn for any model which user script might create, MANDATORY parameter
-	 * @returns {*}
-	 */
-	function registerUserModel(schemaExtend, opts) {
-
-		if (userModel) {    //if it was already assigned, we throw
-			throw new Error('There can only be one user model');
-		}
-		var userSchema = require('./utils/user-model-base');
-		_.extend(userSchema, schemaExtend);
-		userModel = MRModel.call(mongoose, 'user', userSchema, opts);
-        toCallOnCreate.push(userModel._exposeCallback);
-
-        return userModel;
-	}
-
+module.exports = {
     /**
      *
-     * @param hn
-     * @param {Object} user must have _id nad other mongoDB properties
+     * @param mongoose
+     * @returns {{model: regNewModel, userModel: registerUserModel, authUser: authUser}}
      */
-    function authUser(hn, user) {
-//        logger.info("Authenticated socket with id: " + hn.id);
-        hn.user = user;
-    }
+    init: function (mongoose) {
 
-    return {model: regNewModel, userModel: registerUserModel, authUser: authUser};
-};
+        /**
+         * @param {String} name
+         * @param {Object} schema
+         * @param {Object} opts
+         * @param {Function} opts.checkPermission function which should return true/false depending if the connected socket has/hasn't priviliges
+         * @param {Object} opts.permissions with 4 properties: "C", "R","U", "D" which each represents one type of operation,
+         *                                  values should be numbers indicating which level of privilige is needed
 
-/**
- * @param {Manager} io socket.io manager
- * @param {Object} app Express.js app object
- * @returns {SocketNamespace} master socket namespace
- */
-var createServer = function (io, app) {
+         * @returns {MRModel}
+         */
+        function regNewModel(name, schema, opts) {
 
-    app.get('/moonridge-angular-client.js', function (req, res) { //exposed client file
-        res.sendfile('node_modules/moonridge/client/moonridge-angular-client.js');
-    });
+            var model = MRModel.apply(mongoose, arguments);
+            toCallOnCreate.push(model._exposeCallback);
 
-    app.get('/moonridge-angular-client-rpcbundle.js', function (req, res) { //exposed client file
-        res.sendfile('node_modules/moonridge/built/moonridge-angular-client-rpcbundle.js');
-    });
-
-    app.get('/moonridge-angular-client-rpcbundle.min.js', function (req, res) { //exposed client file
-        res.sendfile('node_modules/moonridge/built/moonridge-angular-client-rpcbundle.min.js');
-    });
-    var allQueries = [];
-
-    app.get('/health', function(req, res){
-
-        var allModels = {};
-        var index = allQueries.length;
-        while(index--) {
-            var modelQueriesForSerialization = {};
-            var model = allQueries[index];
-            for (var LQ in model.queries) {
-                var listenerCount = Object.keys(model.queries[LQ].listeners).length;
-                modelQueriesForSerialization[LQ] = listenerCount;
-            }
-            allModels[model.modelName] = modelQueriesForSerialization;
-
+            return model;
         }
-        res.send({
-            pid: process.pid,
-            memory: process.memoryUsage(),
-            uptime: process.uptime(),   //in seconds
-            liveQueries: allModels  //key is LQ.clientQuery and value is number of listening clients
+
+        /**
+         *
+         * @param schemaExtend
+         * @param {Object} opts
+         * @param {Function} opts.authFn will be set as default authFn for any model which user script might create, MANDATORY parameter
+         * @returns {*}
+         */
+        function registerUserModel(schemaExtend, opts) {
+
+            if (userModel) {    //if it was already assigned, we throw
+                throw new Error('There can only be one user model');
+            }
+            var userSchema = require('./utils/user-model-base');
+            _.extend(userSchema, schemaExtend);
+            userModel = MRModel.call(mongoose, 'user', userSchema, opts);
+            toCallOnCreate.push(userModel._exposeCallback);
+
+            return userModel;
+        }
+
+        /**
+         *
+         * @param hn
+         * @param {Object} user must have _id nad other mongoDB properties
+         */
+        function authUser(hn, user) {
+//        logger.info("Authenticated socket with id: " + hn.id);
+            hn.user = user;
+        }
+
+        return {model: regNewModel, userModel: registerUserModel, authUser: authUser};
+    },
+    /**
+     * @param {Manager} io socket.io manager
+     * @param {Object} app Express.js app object
+     * @returns {SocketNamespace} master socket namespace
+     */
+    createServer: function (io, app) {
+
+        app.get('/moonridge-angular-client.js', function (req, res) { //exposed client file
+            res.sendfile('node_modules/moonridge/client/moonridge-angular-client.js');
         });
-    });
 
-    var socketNamespace = rpc.createServer(io, {expressApp: app});
+        app.get('/moonridge-angular-client-rpcbundle.js', function (req, res) { //exposed client file
+            res.sendfile('node_modules/moonridge/built/moonridge-angular-client-rpcbundle.js');
+        });
 
-    toCallOnCreate.forEach(function (CB) {
-       allQueries.push(CB());   //return object containing modelName and liveQueries Object for that model
-    });
+        app.get('/moonridge-angular-client-rpcbundle.min.js', function (req, res) { //exposed client file
+            res.sendfile('node_modules/moonridge/built/moonridge-angular-client-rpcbundle.min.js');
+        });
+        var allQueries = [];
 
-    return socketNamespace;
+        var socketNamespace = rpc.createServer(io, {expressApp: app});
 
-//    rpc.expose('Moonridge', {
-//        getModels: function () {
-//			return mongoose.models;
-//        }
-//    });
+        toCallOnCreate.forEach(function (CB) {
+            allQueries.push(CB());   //return object containing modelName and liveQueries Object for that model
+        });
 
+        rpc.expose('Moonridge_admin', {
+            getHealth: function () {
+                var allModels = {};
+                var index = allQueries.length;
+                while(index--) {
+                    var modelQueriesForSerialization = {};
+                    var model = allQueries[index];
+                    for (var LQ in model.queries) {
+                        var listenerCount = Object.keys(model.queries[LQ].listeners).length;
+                        modelQueriesForSerialization[LQ] = listenerCount;
+                    }
+                    allModels[model.modelName] = modelQueriesForSerialization;
+
+                }
+                return {
+                    pid: process.pid,
+                    memory: process.memoryUsage(),
+                    uptime: process.uptime(),   //in seconds
+                    liveQueries: allModels  //key is LQ.clientQuery and value is number of listening clients
+                };
+            }
+        });
+
+        return socketNamespace;
+
+    }
 };
-
-module.exports = {init: init, createServer: createServer};

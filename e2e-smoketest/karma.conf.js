4b034fa82da604cc3461668b2794a607e6330f28
Jiří Špác <capajj@gmail.com>
2014-08-02 21:53:40 +0200

    added test and karma conf

---
+++ e2e-smoketest/karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
+
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
+
+        // list of files / patterns to load in the browser
+        files: ['e2e-smoketest/mr-test-ctrl.js'],
+
+        // list of files / patterns to exclude
+        exclude: [],
+
+        // web server port
+        port: 8066,
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS']
+        //    browsers: ['Chrome','Safari','Firefox','Opera','ChromeCanary'],
+
+    });
+};

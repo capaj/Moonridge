42d08557688fd16bfa56044be9cebc4b2c540990
Jiri Spac <jiri.spac@ys.cz>
2014-10-29 23:57:37 +0100

    jspm initialized in example app

---
+++ e2e-smoketest/config.js
@@ -0,0 +1,19 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@^1.3.0"
+  }
+});
+
+System.config({
+  "versions": {
+    "github:angular/bower-angular": "1.3.0"
+  }
+});
+

f2e5fbdf757739e55707e466c4128f5d0a8e2a71
Jiří Špác <capajj@gmail.com>
2014-11-05 23:56:12 +0100

    config according to jspm json

--- e2e-smoketest/config.js
+++ e2e-smoketest/config.js
@@ -7,13 +7,16 @@ System.config({
 
 System.config({
   "map": {
-    "socket.io-client": "/socket.io/socket.io",
-    "github:urish/angular-moment": "github:urish/angular-moment@^0.8.2",
-    "text": "github:systemjs/plugin-text@^0.0.2",
-    "css": "github:systemjs/plugin-css@^0.1.0",
-    "jquery": "github:components/jquery@^2.1.1",
     "angular": "github:angular/bower-angular@^1.3.1",
-    "angular-animate": "github:angular/bower-angular-animate@^1.3.1"
+    "angular-animate": "github:angular/bower-angular-animate@^1.3.1",
+    "css": "github:systemjs/plugin-css@^0.1.0",
+    "github:urish/angular-moment": "github:urish/angular-moment@^0.8.2",
+    "jquery": "github:components/jquery@^2.1.1",
+    "socket.io-client": "/socket.io/socket.io",
+    "text": "github:systemjs/plugin-text@^0.0.2",
+    "github:angular/bower-angular-animate@1.3.1": {
+      "angular": "github:angular/bower-angular@^1.3.1"
+    }
   }
 });
 

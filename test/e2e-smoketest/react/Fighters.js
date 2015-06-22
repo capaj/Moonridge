a153a4395ad3a8fe7bd71083ccdae2dcd9e51261
Jiří Špác <capajj@gmail.com>
2015-06-22 06:53:21 +0200

    react example improved

--- test/e2e-smoketest/react/Fighters.js
+++ test/e2e-smoketest/react/Fighters.js
@@ -2,26 +2,33 @@ import React from 'react';
 
 var {Component} = React;
 
+import {default as $MR} from 'moonridge-client';
+
+var MRB = $MR({url: 'http://localhost:8080'});  //true indicates, that this backend should be used by default
+MRB.socket.on('disconnect', function() {
+	console.log("Ha disconnected!");
+});
+
+
 export default class Fighters extends Component {
 	constructor(...props) {
 		super(...props);
-		this.state = {
-			LQ : [
-				{key: '1', title: 'Item 1', description: 'So cool', voteCount: 49},
-				{key: '2', title: 'Item 2', description: 'Meh', voteCount: 11},
-				{key: '3', title: 'Item 3', description: 'Great', voteCount: 30}
-				]
-		};
+		var fighter = MRB.model('fighter');
+		this.state = {docs: []};
+		var LQ = fighter.liveQuery().sort('health').exec();
+		LQ.on('any', () => {
+			this.setState({docs: LQ.docs});
+		});
 	}
 
 	render() {
-		var feedItems = this.state.LQ.map(function(item) {
-			return <div>{item.description}</div>;
+		var fighters = this.state.docs.map(function(item) {
+			return <div key={item._id}>{item.name}</div>;
 		});
 
 		return (
 			<ul className="list-group container">
-				{feedItems}
+				{fighters}
 			</ul>
 		);
 	}

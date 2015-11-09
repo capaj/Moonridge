308befe62b9711af13720d8455f13046fa904d6d
Jiri Spac <capajj@gmail.com>
2015-11-09 23:04:23 +0000

    fixed compatibility with latest socket.io-rpc
    bumped

--- moonridge.js
+++ moonridge.js
@@ -1,13 +1,13 @@
-var RPC = require('socket.io-rpc');
-var _ = require('lodash');
-var debug = require('debug')('moonridge:server');
-var MRModel = require('./mr-server-model');
-var userModel;
-var moonridgeSingleton;
+var RPC = require('socket.io-rpc')
+var _ = require('lodash')
+var debug = require('debug')('moonridge:server')
+var MRModel = require('./mr-server-model')
+var userModel
+var moonridgeSingleton
 
-var express = require('express');
-var models = {};
-var mongoose = require('mongoose');
+var express = require('express')
+var models = {}
+var mongoose = require('mongoose')
 /**
  *
  * @param {String} connString to mongoDB
@@ -20,14 +20,14 @@ function connect(connString) {
 		mongoose.connect(connString, function(err) {
 			// if we failed to connect, abort
 			if (err) {
-				throw err;
+				throw err
 			} else {
-				debug("DB connected succesfully");
+				debug("DB connected succesfully")
 			}
-		});
+		})
 		mongoose.connection.on('error', function(err) {
-			console.error('MongoDB error: %s', err);
-		});
+			console.error('MongoDB error: %s', err)
+		})
 	}
 }
 /**
@@ -42,9 +42,9 @@ function connect(connString) {
  */
 function regNewModel(name, schema, opts) {
 
-	var model = MRModel.apply(moonridgeSingleton, arguments);
-	models[name] = model;
-	return model;
+	var model = MRModel.apply(moonridgeSingleton, arguments)
+	models[name] = model
+	return model
 }
 
 /**
@@ -56,14 +56,14 @@ function regNewModel(name, schema, opts) {
 function registerUserModel(schemaExtend, opts) {
 
 	if (userModel) {    //if it was already assigned, we throw
-		throw new Error('There can only be one user model and it was already registered');
+		throw new Error('There can only be one user model and it was already registered')
 	}
-	var userSchema = require('./utils/user-model-base');
-	_.extend(userSchema, schemaExtend);
-	userModel = MRModel.call(moonridgeSingleton, 'user', userSchema, opts);
-	models['user'] = userModel;
+	var userSchema = require('./utils/user-model-base')
+	_.extend(userSchema, schemaExtend)
+	userModel = MRModel.call(moonridgeSingleton, 'user', userSchema, opts)
+	models['user'] = userModel
 
-	return userModel;
+	return userModel
 }
 
 /**
@@ -74,60 +74,60 @@ function registerUserModel(schemaExtend, opts) {
  * @returns {{rpcNsp: (Emitter), io: {Object}, server: http.Server}}
  */
 function bootstrap() {
-	var server = RPC.apply(null, arguments);
-	var io = server.io;
+	var server = RPC.apply(null, arguments)
+	var io = server.io
 
-	var allQueries = [];
+	var allQueries = []
 
 	io.on('connection', function(socket) {
-		socket.registeredLQs = [];
+		socket.registeredLQs = []
 		socket.on('disconnect', function() {
 			//clearing out liveQueries listeners
 			for (var LQId in socket.registeredLQs) {
-				var LQ = socket.registeredLQs[LQId];
-				LQ.removeListener(socket);
+				var LQ = socket.registeredLQs[LQId]
+				LQ.removeListener(socket)
 			}
-		});
-	});
+		})
+	})
 
 	Object.keys(models).forEach(function(modelName) {
-		var model = models[modelName];
-		model._exposeCallback(server);
-	});
+		var model = models[modelName]
+		model._exposeCallback(server)
+	})
 
 	io.use(function(socket, next) {
-		socket.moonridge = {user: {privilege_level: 0}}; //default privilege level for any connected client
-		next();
-	});
+		socket.moonridge = {user: {privilege_level: 0}} // default privilege level for any connected client
+		next()
+	})
 
-	server.expressApp.get('/MR/models.js', function(req, res) {
-		res.type('application/javascript; charset=utf-8');
-		var modelNames = Object.keys(models);
+	server.allModelGetHandler = function (req, res) {
+		res.type('application/javascript charset=utf-8')
+		var modelNames = Object.keys(models)
 		var clSideScript = 'module.exports = function(MR) {' +
-			'var modelsHash = {};' +
-			'var models = ' + JSON.stringify(modelNames) + ';' +
-			'models.forEach(function(modelName) {modelsHash[modelName] = MR.model(modelName);});' +
-			'return modelsHash;' +
-			'};';
-		res.send(clSideScript);
-		res.end();
-	});
+			'var modelsHash = {}' +
+			'var models = ' + JSON.stringify(modelNames) + '' +
+			'models.forEach(function(modelName) {modelsHash[modelName] = MR.model(modelName)})' +
+			'return modelsHash' +
+			'}'
+		res.send(clSideScript)
+		res.end()
+	}
 
-	if (server.expressApp.get('env') === 'development') {
+	if (process.env.MOONRIDGE_HEALTH === '1') {
 		// this reveals any data that you use in queries to the public, so it should not be used in production when dealing with sensitive data
 
 		server.expose({
 			MR: {
 				getHealth: function() {
-					var allModels = {};
-					var index = allQueries.length;
+					var allModels = {}
+					var index = allQueries.length
 					while (index--) {
-						var modelQueriesForSerialization = {};
-						var model = allQueries[index];
+						var modelQueriesForSerialization = {}
+						var model = allQueries[index]
 						for (var LQ in model.queries) {
-							modelQueriesForSerialization[LQ] = Object.keys(model.queries[LQ].listeners).length;
+							modelQueriesForSerialization[LQ] = Object.keys(model.queries[LQ].listeners).length
 						}
-						allModels[model.modelName] = modelQueriesForSerialization;
+						allModels[model.modelName] = modelQueriesForSerialization
 
 					}
 					return {
@@ -135,14 +135,14 @@ function bootstrap() {
 						memory: process.memoryUsage(),
 						uptime: process.uptime(),   //in seconds
 						liveQueries: allModels  //key is LQ.clientQuery and value is number of listening clients
-					};
+					}
 				}
 			}
-		});
+		})
 
 	}
 
-	return server;
+	return server
 }
 
 moonridgeSingleton = {
@@ -152,6 +152,6 @@ moonridgeSingleton = {
 	mongoose: mongoose,
 	models: models,
 	connect: connect
-};
+}
 
-module.exports = moonridgeSingleton;
+module.exports = moonridgeSingleton
--- test/e2e-smoketest/server.js
+++ test/e2e-smoketest/server.js
@@ -1,69 +1,72 @@
-var _ = require('lodash');
-var locals = require('./localVariables.json');
+var _ = require('lodash')
+var locals = require('./localVariables.json')
 
-var MR = require('../../moonridge');
-var staticMW = require('express').static;
+var MR = require('../../moonridge')
+var staticMW = require('express').static
 
-MR.connect(locals.connString);
-var mongoose = MR.mongoose;
-mongoose.set('debug', true);
-var dbInit = require('./db-init');
-dbInit(MR);
-var server = MR.bootstrap(8080);
+MR.connect(locals.connString)
+var mongoose = MR.mongoose
+mongoose.set('debug', true)
+var dbInit = require('./db-init')
+dbInit(MR)
+var app = require('express')()
+var server = require('http').Server(app)
 
-var app = server.expressApp;
-app.use(require('morgan')('dev'));
+var rpcServer = MR.bootstrap(server)
 
-app.use(staticMW('./Moonridge-client/'));//only needed when moonridge is not as npm module
+app.use(require('morgan')('dev'))
 
-app.use(staticMW('./test/e2e-smoketest/'));
-app.use(staticMW('./test/e2e-smoketest/angular'));
-app.use(staticMW('./test/e2e-smoketest/aurelia'));
-app.use(staticMW('./test/e2e-smoketest/react'));
+app.use(staticMW('./Moonridge-client/')) // only needed when moonridge is not as npm module
 
-var assignUserToASocket = function(socket) {
-	return function(user) {
-		if (user) {
-			console.log("Authenticated user: ", user);
-			socket.moonridge.user = user;
-		} else {
-			console.warn('did not find such user');
-		}
-		return user;
-	};
-};
+app.use(staticMW('./test/e2e-smoketest/'))
+app.use(staticMW('./test/e2e-smoketest/angular'))
+app.use(staticMW('./test/e2e-smoketest/aurelia'))
+app.use(staticMW('./test/e2e-smoketest/react'))
 
-server.io.use(function(socket, next) {	//example of initial authorization
-	//it is useful only for apps which require user authentication by default
-	var authObj = socket.handshake.query;
-	var userName = authObj.nick;
+var assignUserToASocket = function (socket) {
+  return function (user) {
+    if (user) {
+      console.log('Authenticated user: ', user)
+      socket.moonridge.user = user
+    } else {
+      console.warn('did not find such user')
+    }
+    return user
+  }
+}
 
-	console.log("user wants to authorize: " + userName );
-	console.log("socket: ", socket.handshake.query);
-	console.log("socket.id: " + socket.id);
-	var userModel = mongoose.model('user');
-	userModel.findOne({name: userName}).exec().then(function(user) {
-		assignUserToASocket(socket)(user);
-		next();
-	}, function (err) {
-		console.log("auth error " + err);
-		next(err);
-	});
+rpcServer.io.use(function (socket, next) { // example of initial authorization
+  // it is useful only for apps which require user authentication by default
+  var authObj = socket.handshake.query
+  var userName = authObj.nick
 
-});
+  console.log('user wants to authorize: ' + userName)
+  console.log('socket: ', socket.handshake.query)
+  console.log('socket.id: ' + socket.id)
+  var userModel = mongoose.model('user')
+  userModel.findOne({name: userName}).exec().then(function (user) {
+    assignUserToASocket(socket)(user)
+    next()
+  }, function (err) {
+    console.log('auth error ' + err)
+    next(err)
+  })
 
-server.expose({
-	MR: {
-		authorize: function(data) {	//example of a later authorization, typical for any public facing apps
-			var socket = this;
-			var userModel = mongoose.model('user');
-			console.log('data', data);
+})
 
-			return userModel.findOne({name: data.nick}).exec().then(assignUserToASocket(socket), function (err) {
-				console.log("auth error " + err);
-			});
-		}
-	}
-});
+rpcServer.expose({
+  MR: {
+    authorize: function (data) { // example of a later authorization, typical for any public facing apps
+      var socket = this
+      var userModel = mongoose.model('user')
+      console.log('data', data)
 
-module.exports = MR;
+      return userModel.findOne({name: data.nick}).exec().then(assignUserToASocket(socket), function (err) {
+        console.log('auth error ' + err)
+      })
+    }
+  }
+})
+server.listen(8080)
+
+module.exports = MR

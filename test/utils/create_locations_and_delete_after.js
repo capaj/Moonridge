b5548743a596bf3d5c2db65d4f8c2deefa5f6ed4
Jiri Spac <capajj@gmail.com>
2016-01-06 02:00:39 +0100

    just updated deps and fixed codestyle on some of the test scripts

--- test/utils/create_locations_and_delete_after.js
+++ test/utils/create_locations_and_delete_after.js
@@ -4,21 +4,21 @@ var all
 var locationsModel
 
 module.exports = {
-	before:  function(model) {
-		locationsModel = model
-		return mr.authorize({nick: 'admin'}).then(function() {
-			all = Promise.all([
-				locationsModel.create({loc: [1, 2]}),
-				locationsModel.create({loc: [2, 3]}),
-				locationsModel.create({loc: [5, 3]})
-			])
-			return all
-		})
-	},
-	after: function() {
-		return all.then(function(locations){
-			return Promise.all(locations.map(locationsModel.remove))
-		})
-	}
+  before:  function(model) {
+    locationsModel = model
+    return mr.authorize({nick: 'admin'}).then(function() {
+      all = Promise.all([
+        locationsModel.create({loc: [1, 2]}),
+        locationsModel.create({loc: [2, 3]}),
+        locationsModel.create({loc: [5, 3]})
+      ])
+      return all
+    })
+  },
+  after: function() {
+    return all.then(function(locations){
+      return Promise.all(locations.map(locationsModel.remove))
+    })
+  }
 
 }
--- test/utils/run_server_client.js
+++ test/utils/run_server_client.js
@@ -1,3 +1,4 @@
+/* eslint-env node, mocha */
 'use strict'
 var cp = require('child_process')
 
@@ -5,35 +6,34 @@ var server = cp.fork('./test/e2e-smoketest/server.js')
 
 var $MR = require('../../Moonridge-client/moonridge-client')
 
-//Moonridge backend
+// Moonridge backend
 var mr = $MR({url: 'http://localhost:8080', hs: {query: 'nick=testUser'}})
 const mongojs = require('mongojs')
 
-let models = ['fighters', 'battles', 'users', 'locations', 'sample_models'];
+let models = ['fighters', 'battles', 'users', 'locations', 'sample_models']
 const db = mongojs('mrtest', models)
 let allDone = false
 
-mr.socket.on('disconnect', function() {
-	if (!allDone) {
-		throw new Error('Disconnection should not occur')
-	}
+mr.socket.on('disconnect', function () {
+  if (!allDone) {
+    throw new Error('Disconnection should not occur')
+  }
 })
 
 module.exports = {
-	server: server,
-	client: mr
+  server: server,
+  client: mr
 }
 
-after(function(done) {
-	allDone = true
-	models.forEach((collName, i) => {
-		db[collName].drop(() => {
-			if (i === 4) {
-				server.on('close', done)
-				console.log("killing the test server")
-				return server.kill()
-			}
-		})
-	})
-
+after(function (done) {
+  allDone = true
+  models.forEach((collName, i) => {
+    db[collName].drop(() => {
+      if (i === 4) {
+        server.on('close', done)
+        console.log('killing the test server')
+        return server.kill()
+      }
+    })
+  })
 })
